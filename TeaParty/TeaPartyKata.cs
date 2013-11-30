4224c868a84ee821a76e7fdc32a0e821960922f3
mattbettinson <mjbettinson@gmail.com>
2013-11-30 16:57:40 +0000

    Adding TeaParty kata

---
+++ TeaParty/TeaPartyKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaParty
+{
+    public class TeaPartyKata: ITeaPartyKata
+    {
+        public string Welcome(string lastName, bool isWoman, bool isSir)
+        {
+            return string.Format("Hello {0} {1}", isWoman ? "Ms." : (isSir ? "Sir" : "Mr."), lastName);
+        }
+    }
+}
---
+++ TeaParty/TestTeaPartyKata.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaParty
+{
+    [TestFixture]
+    public class TestTeaPartyKata
+    {
+        [TestCase("Austen", true, false, "Hello Ms. Austen")]
+        [TestCase("Orwell", false, false, "Hello Mr. Orwell")]
+        [TestCase("Newton", false, true, "Hello Sir Newton")]
+        public void TeaPartyKata_Welcome(string lastname, bool isWoman, bool isKnighted, string expected)
+        {
+            // Arrange
+            var teaPartyKata = new TeaPartyKata();
+
+            // Act
+            var actual = teaPartyKata.Welcome(lastname, isWoman, isKnighted);
+            
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
